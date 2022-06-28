d849e9cd917a5746e0e38724051387dea4bc421e
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2022-06-28 09:57:53 -0700

    auto sync for version 2022.1

--- CS/GroupByTimeSpan/Form1.cs
+++ CS/GroupByTimeSpan/Form1.cs
@@ -1,70 +1,80 @@
 using System;
 using System.Drawing;
 using System.Windows.Forms;
+using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DataAccess.Sql;
 using DevExpress.XtraReports.UI;
-// ...
 
 namespace GroupByTimeSpan {
     public partial class Form1: Form {
         public Form1() {
             InitializeComponent();
         }
-
         public XtraReport CreateDataGroupingReport() {
             // Create a report, and bind it to a data source.
             XtraReport report = new XtraReport();
-            nwindDataSet ds = new nwindDataSet();
-            new nwindDataSetTableAdapters.OrdersTableAdapter().Fill(ds.Orders);
-            report.DataSource = ds;
+
+            SqlDataSource sqlDataSource = new SqlDataSource(
+                new SQLiteConnectionParameters() {
+                    FileName = "nwind.db",
+                    Password = null
+                });
+            SelectQuery queryOrders = SelectQueryFluentBuilder
+                .AddTable("Orders")
+                .SelectAllColumnsFromTable()
+                .Filter("[OrderDate] > #2016-01-01#")
+                .Build("Orders");
+            sqlDataSource.Queries.Add(queryOrders);
+            sqlDataSource.Fill();
+
+            report.DataSource = sqlDataSource;
             report.DataMember = "Orders";
 
-            // Create a detail band and add it to the report.
+            // Create a Detail band, and add it to the report.
             DetailBand detailBand = new DetailBand();
             detailBand.Height = 20;
             report.Bands.Add(detailBand);
 
-            // Create a group header band and add it to the report.
+            // Create a Group Header band, and add it to the report.
             GroupHeaderBand ghBand = new GroupHeaderBand();
             ghBand.Height = 20;
             report.Bands.Add(ghBand);
 
-            // Create a calculated field, and add it to the report's collection
+            // Create a calculated field, and add it to the collection.
             CalculatedField calcField = new CalculatedField(report.DataSource, report.DataMember);
-            report.CalculatedFields.Add(calcField);
-
-            // Define its name, field type and expression.
-            // Note that numerous built-in date-time functions are supported.
-            calcField.Name = "dayOfWeek";
+            calcField.Name = "calcDayOfWeek";
             calcField.FieldType = FieldType.None;
             calcField.Expression = "GetDayOfWeek([OrderDate])";
+            report.CalculatedFields.Add(calcField);
 
-            // Define the calculated field as 
-            // a grouping criteria for the group header band.
+            // Specify the calculated field as a grouping basis.
             GroupField groupField = new GroupField();
-            groupField.FieldName = "dayOfWeek";
+            groupField.FieldName = "calcDayOfWeek";
             ghBand.GroupFields.Add(groupField);
-           
-            // Create two data-bound labels, and add them to 
-            // the corresponding bands.
+
+            // Create two data-bound labels and add them to the related report bands.
             XRLabel ghLabel = new XRLabel();
-            ghLabel.DataBindings.Add("Text", report.DataSource, "OrderDate", "{0:dddd}");
-            ghLabel.BackColor = Color.Red;
+            ghLabel.DataBindings.Add("Text", report.DataSource, "Orders.OrderDate", "{0:dddd}");
+            ghLabel.BackColor = Color.SteelBlue;
+            ghLabel.ForeColor = Color.White;
             ghBand.Controls.Add(ghLabel);
 
             XRLabel detailLabel = new XRLabel();
-            detailLabel.DataBindings.Add("Text", report.DataSource, "OrderDate", "{0:MM/dd/yyyy}");
+            detailLabel.DataBindings.Add("Text", report.DataSource, "Orders.OrderDate", "{0:MM/dd/yyyy}");
             detailLabel.ProcessDuplicatesTarget = ProcessDuplicatesTarget.Value;
             detailLabel.ProcessDuplicatesMode = ProcessDuplicatesMode.Suppress;
+            detailLabel.LeftF = 20;
             detailBand.Controls.Add(detailLabel);
 
             return report;
         }
-
         private void button1_Click(object sender, EventArgs e) {
             // Create a report grouped by days of week, 
             // and show its print preview.
             XtraReport report = CreateDataGroupingReport();
+            report.SaveLayoutToXml("test.repx");
             report.ShowPreview();
+
         }
 
     }
